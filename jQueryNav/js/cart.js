67d976ca4ed70c5fba6d56487ffc12db837c8e58
wuyeyang212 <289752132@qq.com>
2018-03-28 20:34:23 +0800

    2018-3-28

---
+++ jQueryNav/js/cart.js
@@ -0,0 +1,72 @@
+$(function (){
+    var $allCheckbox = $('input[type="checkbox"]'),//这个是全部的全局盒子
+        $wholeChexbox = $('.quan_check'),         //这个是全选的盒子
+        $cartBox = $('.b-c-box')
+        $sonCheckBox = $('.son_check');
+    $allCheckbox.click(function () {
+        if ($(this).is(':checked')) {
+            $(this).next('label').addClass('mark');
+        } else {
+            $(this).next('label').removeClass('mark')
+        }
+    });
+//===============================================全局全选与单个商品的关系================================
+    $wholeChexbox.click(function(){
+     var $checkboxs = $cartBox.find('input[type="checkbox"]');
+        if ($(this).is(':checked')) {
+            $checkboxs.prop("checked", true);
+            $checkboxs.next('label').addClass('mark');
+        } else {
+            $checkboxs.prop("checked", false);
+            $checkboxs.next('label').removeClass('mark');
+        }
+    })
+    $sonCheckBox.each(function () {
+        $(this).click(function () {
+            if ($(this).is(':checked')) {
+                //判断：所有单个商品是否勾选
+                var len = $sonCheckBox.length;
+                var num = 0;
+                $sonCheckBox.each(function () {
+                    if ($(this).is(':checked')) {
+                        num++;
+                    }
+                });
+                if (num == len) {
+                    $wholeChexbox.prop("checked", true);
+                    $wholeChexbox.next('label').addClass('mark');
+                }
+            } else {
+                //单个商品取消勾选，全局全选取消勾选
+                $wholeChexbox.prop("checked", false);
+                $wholeChexbox.next('label').removeClass('mark');
+            }
+        })
+    })
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+})
