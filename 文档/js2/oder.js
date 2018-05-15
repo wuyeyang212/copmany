ab2712bd641cfca488cd6da70eacdf6603fdba61
wuyeyang212 <289752132@qq.com>
2018-05-15 10:29:55 +0800

    ss03

--- 文档/js2/oder.js
+++ 文档/js2/oder.js
@@ -26,7 +26,7 @@
 			});
 		}, false);
 	});
-
+	
 })(mui, document);
 
 (function ($, doc) {
@@ -38,9 +38,11 @@
 		 * @param {Object} obj 对象
 		 * @param {String} param 属性名
 		 */
-
+		var inde = "";//市
+		var de = "";//区
+		var qu = "";//县
 		var _getParam = function (obj, param) {
-			return obj[param] || '';
+			return obj[param] || "";
 		};
 		var cityPicker3 = new $.PopPicker({
 			layer: 3
@@ -50,15 +52,22 @@
 		var cityResult3 = doc.getElementById('cityResult34');
 		showCityPickerButton.addEventListener('tap', function (event) {
 			cityPicker3.show(function (items) {
+				console.log(items);
 				cityResult3.innerText = _getParam(items[0], 'text') + " " + _getParam(items[1], 'text') + " " + _getParam(items[2], 'text');
+				inde = _getParam(items[0], 'value') ;
+				de = _getParam(items[1], 'value') ;
+				qu = _getParam(items[2], 'value')
+				console.log(inde,de,qu);
 				cityResult3.style.color = "black"
 				//返回 false 可以阻止选择框的关闭
 				//return false;
 			});
 		}, false);
+		
 	});
 
 })(mui, document);
+
 //初始化单页view
 var viewApi = mui('#app').view({
     defaultPage: '#setting'
@@ -72,27 +81,31 @@ mui('.mui-scroll-wrapper').scroll({
 var view = viewApi.view;
 (function($) {
     //处理view的后退与webview后退
-    var oldBack = $.back;
+	var oldBack = $.back;
+	var cityResult3 = document.getElementById('cityResult34');
     $.back = function() {
         if (viewApi.canBack()) { //如果view可以后退，则执行view的后退
             viewApi.back();
         } else { //执行webview后退
             oldBack();
         }
-    };
+	};
+	
     view.addEventListener('pageBeforeShow', function(e) {
         //                console.log(e.detail.page.id + ' beforeShow');
     });
     view.addEventListener('pageShow', function(e) {
-                          console.log(e.detail.page.id + ' show');
+                //   console.log(cityResult3.innerText)
     });
     view.addEventListener('pageBeforeBack', function(e) {
         //                console.log(e.detail.page.id + ' beforeBack');
     });
     view.addEventListener('pageBack', function(e) {
         //                console.log(e.detail.page.id + ' back');
-    });
+	});
+	
 })(mui);
+
 $('.mask').click(function () {
 
 	$(this).css('display', 'none');
