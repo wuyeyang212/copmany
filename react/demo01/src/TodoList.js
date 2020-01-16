939e4c574cf4ea57b796040797a143f32e17fb84
weihuijuan <weihuijuan@weihuijuan.com>
2020-01-16 18:00:57 +0800

    页面的改变

--- react/demo01/src/TodoList.js
+++ react/demo01/src/TodoList.js
@@ -10,6 +10,9 @@ class TodoList extends Component {
         //console.log(store.getState())
         this.state = store.getState()
         this.changeInputValue = this.changeInputValue.bind(this)
+        this.storeChange = this.storeChange.bind(this)
+        this.clickBtn = this.clickBtn.bind(this)
+        store.subscribe(this.storeChange)
     }
     
     render() { 
@@ -18,15 +21,19 @@ class TodoList extends Component {
                 <div style={{margin:'10px'}}> 
                     <Input 
                     placeholder={this.state.inputValue}
-                    onChange={this.changeInputValue} 
+                    onChange={this.changeInputValue}
+                    value={this.state.inputValue} 
                     style = {{width:'250px',marginRight:'10px'}}/>
-                    <Button type="primary">增加</Button>
+                    <Button 
+                        type="primary"
+                        onClick = {this.clickBtn}
+                    >增加</Button>
                 </div>
                 <div style={{margin:'10px',width:'300px'}}>
                     <List
                         bordered
                         dataSource={this.state.list}
-                        renderItem={item=>(<List.Item>{item}</List.Item>)}/>
+                        renderItem={(item,index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}/>
                 </div>
             </div>
          );
@@ -37,6 +44,20 @@ class TodoList extends Component {
             value:e.target.value
         }
         store.dispatch(action) 
+    }
+    clickBtn(){
+        const action = {type:'addItem'}
+        store.dispatch(action)
+    }
+    storeChange(){
+        this.setState(store.getState)
+    }
+    deleteItem(index){
+        const action = {
+            type:'deleteItem',
+            index:index
+        }
+        store.dispatch(action)
     }
 }
 
--- react/demo01/src/store/reducer.js
+++ react/demo01/src/store/reducer.js
@@ -1,4 +1,4 @@
-const defaultState = {
+  const defaultState = {
     inputValue: 'write something',
     list: [
         '早8点开晨会，分配今天的任务代码',
@@ -7,6 +7,30 @@ const defaultState = {
     ]
 }
 export default (state = defaultState, action) => {
-    console.log(state.action)
+    console.log(state,action)
+    //Rediucerl里只能接受state,不能改变state
+    if(action.type==="changeInput"){
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue= action.value 
+        return newState
+    }
+    if(action.type === "addItem"){
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+    if(action.type === "addItem"){
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+    if(action.type === "deleteItem"){
+        let newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.index,1)
+      
+        return newState
+    }
     return state
 }
