95a257a89755457312a1ce89b979074122d6440a
weihuijuan <weihuijuan@weihuijuan.com>
2020-01-17 18:01:58 +0800

    页面

--- react/demo01/src/TodoList.js
+++ react/demo01/src/TodoList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css'
-import {Input,Button,List} from 'antd'
 import store from './store'
-
-
+import {changeInputAction,addItemAction,deleItemAction,getListAction} from './store/actionCreators'
+import TodoListUI from'./TodoListUI'
+import axios from 'axios'
 class TodoList extends Component {
     constructor(props){
         super(props)
@@ -12,51 +11,41 @@ class TodoList extends Component {
         this.changeInputValue = this.changeInputValue.bind(this)
         this.storeChange = this.storeChange.bind(this)
         this.clickBtn = this.clickBtn.bind(this)
+        this.deleteItem = this.deleteItem.bind(this)
         store.subscribe(this.storeChange)
     }
     
     render() { 
         return ( 
-            <div>
-                <div style={{margin:'10px'}}> 
-                    <Input 
-                    placeholder={this.state.inputValue}
-                    onChange={this.changeInputValue}
-                    value={this.state.inputValue} 
-                    style = {{width:'250px',marginRight:'10px'}}/>
-                    <Button 
-                        type="primary"
-                        onClick = {this.clickBtn}
-                    >增加</Button>
-                </div>
-                <div style={{margin:'10px',width:'300px'}}>
-                    <List
-                        bordered
-                        dataSource={this.state.list}
-                        renderItem={(item,index)=>(<List.Item onClick={this.deleteItem.bind(this,index)}>{item}</List.Item>)}/>
-                </div>
-            </div>
+            <TodoListUI
+                inputValue={this.state.inputValue}
+                changeInputValue = {this.changeInputValue}
+                clickBtn = {this.clickBtn}
+                list={this.state.list}
+                deleteItem = {this.deleteItem}
+            />
          );
     }
+    componentDidMount(){
+        axios.get('https://www.easy-mock.com/mock/5cfcce489dc7c36bd6da2c99/xiaojiejie/getList').then((res)=>{
+           const data = res.data
+           const action = getListAction(data)
+           store.dispatch(action)
+        })
+    }
     changeInputValue(e){
-        const action ={
-            type:'changeInput',
-            value:e.target.value
-        }
+        const action =changeInputAction(e.target.value)
         store.dispatch(action) 
     }
     clickBtn(){
-        const action = {type:'addItem'}
+        const action = addItemAction()
         store.dispatch(action)
     }
     storeChange(){
         this.setState(store.getState)
     }
     deleteItem(index){
-        const action = {
-            type:'deleteItem',
-            index:index
-        }
+        const action = deleItemAction(index)
         store.dispatch(action)
     }
 }
